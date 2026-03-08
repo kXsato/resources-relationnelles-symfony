bac70c96868c3e3968beaa069593ac1beeddbb9a
kXsato <cecile.hentsch.pro@gmail.com>
2026-03-08 12:26:53 +0100

    feat: ajouts de l'icon flèches sur le bouton de retour du category crud

--- src/Controller/Dashboard/Admin/CategoryCrudController.php
+++ src/Controller/Dashboard/Admin/CategoryCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Dashboard\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
@@ -13,6 +16,17 @@ class CategoryCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Category::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_EDIT, Action::INDEX)
+            ->add(Crud::PAGE_EDIT, Action::DELETE)
+            ->add(Crud::PAGE_NEW, Action::INDEX)
+            ->disable(Action::SAVE_AND_RETURN)
+            ->update(Crud::PAGE_EDIT, Action::INDEX, fn(Action $a) => $a->setIcon('fas fa-arrow-left'))
+            ->update(Crud::PAGE_NEW, Action::INDEX, fn(Action $a) => $a->setIcon('fas fa-arrow-left'));
     }
 
     public function configureFields(string $pageName): iterable
