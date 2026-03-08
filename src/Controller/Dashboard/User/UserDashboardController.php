333cceb139f566fce2e0a1f40337cabc7cd9d020
Ethan68000 <cathycoulon910@gmail.com>
2026-03-08 15:25:15 +0100

    feat : modification ordre menu et reglage probleme ajout favoris

--- src/Controller/Dashboard/User/UserDashboardController.php
+++ src/Controller/Dashboard/User/UserDashboardController.php
@@ -39,7 +39,8 @@ class UserDashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Mon profil', 'fas fa-user');
         yield MenuItem::linkTo(UserArticleCrudController::class, 'Mes articles', 'fas fa-newspaper');
-        yield MenuItem::linkToLogout('Déconnexion', 'fas fa-sign-out');
         yield MenuItem::linkToRoute('Mes favoris', 'fas fa-star', 'app_favorites');
+        yield MenuItem::linkToLogout('Déconnexion', 'fas fa-sign-out');
+        
     }
 }
