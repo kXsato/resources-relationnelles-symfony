4f5c31f37c5fe94bebb6adbf3954cf9af7addc13
kXsato <cecile.hentsch.pro@gmail.com>
2026-03-08 01:07:11 +0100

    fix : Suppression de l'utilisateur connecter dans l'affiche de la liste des utilisateurs

--- src/Controller/Dashboard/Admin/UserCrudController.php
+++ src/Controller/Dashboard/Admin/UserCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Dashboard\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -11,12 +16,27 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use App\Enum\UserRole;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(private Security $security) {}
+
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $qb = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+        /** @var User $currentUser */
+        $currentUser = $this->security->getUser();
+        $qb->andWhere('entity.id != :currentUser')
+           ->setParameter('currentUser', $currentUser->getId());
+
+        return $qb;
     }
 
     public function configureCrud(Crud $crud): Crud
